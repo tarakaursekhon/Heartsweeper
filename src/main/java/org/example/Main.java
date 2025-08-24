49fe9003cb6011fd4a5462789053b56cb0fbf34a
Tara Sekhon <psyts17@nottingham.ac.uk>
2025-08-24 22:22:05 +0100

    set up squares

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,15 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame screen = new JFrame("heartsweeper <3");
+        screen.setSize(600, 600);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setVisible(true);
     }
+
 }
---
+++ src/main/java/org/example/Square.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Square extends JButton implements ActionListener {
+
+    int x;
+    int y;
+    boolean pressed;
+
+    public Square(int xIn, int yIn) {
+        x = xIn;
+        y = yIn;
+        pressed = false;
+
+        this.addActionListener(this);
+    }
+
+    protected void paintComponent(Graphics gr) {
+        if (!pressed) {
+            gr.setColor(Color.pink);
+            gr.fillRect(0, 0, getWidth(), getHeight());
+            gr.setColor(Color.black);
+            gr.drawRect(0, 0, getWidth(), getHeight());
+        }
+        else {
+            gr.setColor(Color.gray);
+            gr.fillRect(0, 0, getWidth(), getHeight());
+            gr.setColor(Color.black);
+            gr.drawRect(0, 0, getWidth(), getHeight());
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        pressed = true;
+    }
+
+}
